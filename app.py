87d04cf3565c739bf386fda202aa308976cc489b
ylp1472 <pythontestcourse1@gmail.com>
2024-12-30 00:20:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
-
+from routes import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
